688fab4d03a49fc64813eba9b940dfc1b5000b8f
Sameer <smanandhar1@uclan.ac.uk>
2023-09-22 11:00:59 +0100

    Managed to create a graalvm project that generates native executable

---
+++ src/main/java/com/example/nativeapp/NativeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.nativeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nativeapp/NativeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nativeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
